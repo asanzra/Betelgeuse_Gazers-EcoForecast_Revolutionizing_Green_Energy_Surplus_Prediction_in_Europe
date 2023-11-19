4ffef767b701460da2d14c3bca75a6378fe036ce
asanzra <asanz2003@gmail.com>
2023-11-19 04:02:51 +0000

    First working version of data_processing
    -Updated green energy list
    -Added a way to save to pandas
    -Saved pandas to output csv
    
    -Here the output.csv is included for the uploaded LOAD and GEN data

--- src/data_processing.py
+++ src/data_processing.py
@@ -20,28 +20,39 @@ REGIONS = {
 }
 #We do not consider as green and renewable: B01 Biomass, B02-B08 Fossils, B17 Waste
 GREEN_ENERGIES = {
+    'B01' : 'Biowaste',
     'B09' : 'Geothermal',
     'B10' : 'Hydro Pumped Storage',
     'B11' : 'Hydro Run-of-river and poundage',
     'B12' : 'Hydro Water Reservoir',
     'B13' : 'Marine',
-    'B14' : 'Nuclear',
     'B15' : 'Other renewable',
     'B16' : 'Solar',
     'B18' : 'Wind Offshore',
     'B19' : 'Wind Onshore'
 }
 LOAD='Load'
-#AreaID not considered relevant.
+#AreaID not considered relevant, as well as UnitName as all are MAW.
 DATA_POINTS=[
     'StartTime',
-    'EndTime',
-    'UnitName'
+    'EndTime'
 ]
 QUANTITY='quantity'
 # Create a dict containing the columns each country has.
 ENERGY_CATEGORIES=GREEN_ENERGIES.copy()
 ENERGY_CATEGORIES[LOAD]=LOAD
+
+COUNTRIES = {
+    'HU': 'Hungary',
+    'IT': 'Italy',
+    'PO': 'Poland',
+    'SP': 'Spain',
+    'UK': 'United Kingdom',
+    'DE': 'Germay',
+    'DK': 'Denmark',
+    'SE': 'Sweden',
+    'NE': 'Netherlands',
+}
 
 # Checks if a date is in expected format
 def is_expected_date_format(date_string):
@@ -69,6 +80,8 @@ def load_data(file_path):
                 for row in reader:
                     if row['AreaID']!=REGIONS[COUNTRY]:#ELIMINATES BLANK CODE DATA
                             break
+                    if row['UnitName']!='MAW':
+                        raise Exception('One unit name is not MAW but: {row["UnitName"]}')
                     dicti = {} #Store all data in a dict
                     for DATA_POINT in DATA_POINTS:
                         dicti[DATA_POINT] = row[DATA_POINT] #Store relevant vaiables: dates, units...
@@ -77,7 +90,7 @@ def load_data(file_path):
                                 raise Exception("One data point does not have correct form of timezone")
                             #Remove timezone info
                             dicti[DATA_POINT]=datetime.strptime(dicti[DATA_POINT][:-1], "%Y-%m-%dT%H:%M%z").strftime("%Y-%m-%d %H:%M")
-                    dicti[QUANTITY] = row[LOAD]
+                    dicti[QUANTITY] = int(row[LOAD])
                     # if row['AreaID']!=REGIONS[COUNTRY]:
                     #     print(f'In load, country: {COUNTRY} is taking id: {row["AreaID"]}, when it should only take {REGIONS[COUNTRY]}!')
                     data[COUNTRY][LOAD].append(dicti) #Store dict as dataframe
@@ -93,6 +106,8 @@ def load_data(file_path):
                     for row in reader:
                         if row['AreaID']!=REGIONS[COUNTRY]:#ELIMINATES BLANK CODE DATA
                             break
+                        if row['UnitName']!='MAW':
+                            raise Exception('One unit name is not MAW but: {row["UnitName"]}')
                         dicti = {} #Store all data in a dict
                         for DATA_POINT in DATA_POINTS:
                             dicti[DATA_POINT] = row[DATA_POINT] #Store relevant vaiables: dates, units...
@@ -101,15 +116,12 @@ def load_data(file_path):
                                     raise Exception("One data point does not have correct form of timezone")
                                 #Remove timezone info
                                 dicti[DATA_POINT]=datetime.strptime(dicti[DATA_POINT][:-1], "%Y-%m-%dT%H:%M%z").strftime("%Y-%m-%d %H:%M")
-                        dicti[QUANTITY] = row[QUANTITY]
+                        dicti[QUANTITY] = int(row[QUANTITY])
                         # if row['AreaID']!=REGIONS[COUNTRY]:
                         #     print(f'In energy:{ENERGY}, country: {COUNTRY} is taking id: {row["AreaID"]}, when it should only take {REGIONS[COUNTRY]}!')
                         data[COUNTRY][ENERGY].append(dicti) #Store dict as dataframe
-                    if COUNTRY == "IT" and ENERGY=="B10":
-                        print(data['IT']['B10'])
                     data[COUNTRY][ENERGY] = sorted(data[COUNTRY][ENERGY], key=lambda x: x["StartTime"])
     df=data
-    print(data['IT']['B10'])
     return df
 
 # # Define your date range
@@ -126,48 +138,100 @@ def load_data(file_path):
 #     current_date += hour_delta
 
 
+def get_exact_hour(hour_datetime):
+    hour={
+        'year' : hour_datetime.year,
+        'month' : hour_datetime.month,
+        'day' : hour_datetime.day,
+        'hour' : hour_datetime.hour
+        }   
+    return f'{hour["year"]}-{hour["month"]}-{hour["day"]} {hour["hour"]}'
+
+def has_duplicates(lst):
+    return len(lst) != len(set(lst))
+
 def clean_data(df):
     # TODO: Handle missing values, outliers, etc.
     #First we need to only get 1h intervals
+    df_clean={}
     for COUNTRY in REGIONS:
         if basic_info:
             print(f'Cleaning country: {COUNTRY}...')
         for ENERGY in ENERGY_CATEGORIES:
             if extra_info:
                 print(f'Cleaning column: {ENERGY} for country: {COUNTRY}')
-            for i, dicti in enumerate(df[COUNTRY][ENERGY]):
-                start_datetime = datetime.strptime(dicti['StartTime'], "%Y-%m-%d %H:%M")
-                new_end_datetime = datetime.strptime(dicti['EndTime'], "%Y-%m-%d %H:%M")
-                time_difference = new_end_datetime - start_datetime #Delta time for this new interval
-                if i != 0:
-                    if end_datetime != start_datetime: #If the new start time is not the old end time
-                        hours_time_difference = time_difference.total_seconds() / 3600
-                        if hours_time_difference < 1:
-                            #Buscar datos en esa hora y hacer media
-                        else:
-                            #
-                        raise Exception(f'New end time not same as old start time in energy: {ENERGY} for country: {COUNTRY}'+
-                                        f'\n Old end time: {end_datetime}, new start time: {start_datetime}')
-                end_datetime = new_end_datetime
-                if i == 0:
-                    delta_time = time_difference
-                else:
-                    if time_difference != delta_time: #If the new delta time is not as all other delta times
-                        raise Exception(f'Delta time not constant in energy: {ENERGY} for country: {COUNTRY}'+
-                                        f'\n First delta time: {delta_time}, this one:{time_difference}')
-                
 
-    df_clean=None #Linea para que corra
+            #Substitute data saved grouping it by same start hour
+            data_in_each_start_hour={}
+            for dicti in df[COUNTRY][ENERGY]:
+                start_datetime = datetime.strptime(dicti['StartTime'], "%Y-%m-%d %H:%M")
+                end_datetime = datetime.strptime(dicti['EndTime'], "%Y-%m-%d %H:%M")
+                time_difference = end_datetime - start_datetime #Delta time for this new interval
+                minutes = round(time_difference.total_seconds() / 60)
+                if minutes != 15 and minutes != 30 and minutes != 60: #if minute interval is not 15,30 or 60
+                    raise Exception('One time interval is not 15, 30 or 60 minutes but: {minutes}')
+                start_hour=get_exact_hour(start_datetime)
+                end_hour=get_exact_hour(end_datetime)
+                if start_hour != end_hour and end_datetime.minute != 0 : #if data spans over several hours and not to exactlt next hour
+                    raise Exception('Interval spans over different hours and last time is: {end_datetime.strftime("%Y-%m-%d %H:%M:%S")}')
+                dicti['StartTime']=start_datetime
+                dicti['EndTime']=end_datetime
+                dicti['difference']=time_difference
+                if not start_hour in data_in_each_start_hour: #if a list for this hour exists
+                    data_in_each_start_hour[start_hour] = [dicti] #create list containing data in this hour
+                else:
+                    data_in_each_start_hour[start_hour].append(dicti) #append to list
+            df[COUNTRY][ENERGY]=[]
+            #Manage each hour, do averages or complete data
+            for start_hour, hour_data in data_in_each_start_hour.items():
+                start_times = [minute_data['StartTime'] for minute_data in hour_data]
+                if has_duplicates(start_times):
+                    raise Exception(f'Two start times are the same for this list of start times: {[dt.strftime("%Y-%m-%d %H:%M") for dt in start_times]}')
+                end_times = [minute_data['EndTime'] for minute_data in hour_data]
+                if has_duplicates(end_times):
+                    raise Exception(f'Two end times are the same for this list of end times: {[dt.strftime("%Y-%m-%d %H:%M") for dt in end_times]}')
+                second_differences = [minute_data['difference'].total_seconds() for minute_data in hour_data]
+                quantities = [minute_data['quantity'] for minute_data in hour_data]
+                total_second_difference = 0
+                for second_difference in second_differences:
+                    total_second_difference+=second_difference
+                if total_second_difference > 3600: #If the sum of intervals is greater than an hour
+                    raise Exception(f'Total second difference that should be max. 3600s is: {total_second_difference}s.')
+                #Calculate weighted average
+                average_quantity = 0
+                for i, quantity in enumerate(quantities):
+                    average_quantity += quantity + second_differences[i]/total_second_difference
+                hour=start_hour
+                df[COUNTRY][ENERGY].append({hour : average_quantity})
+            df_clean[f'{COUNTRIES[COUNTRY]}_{ENERGY_CATEGORIES[ENERGY]}']=df[COUNTRY][ENERGY] #Append the list of all hours to clean data
     return df_clean
 
 def preprocess_data(df):
     # TODO: Generate new features, transform existing features, resampling, etc.
-    df_processed=None #Linea para que corra
+    df_processed=df
     return df_processed
 
 def save_data(df, output_file):
     # TODO: Save processed data to a CSV file
-    pass
+    data_for_dataframe = {}
+    for main_key, small_key_value_list in df.items():
+        for small_key_value in small_key_value_list:
+            small_key, value = list(small_key_value.items())[0]
+            if small_key not in data_for_dataframe:
+                data_for_dataframe[small_key] = {}
+            data_for_dataframe[small_key][main_key] = value
+
+    # Create a DataFrame from the organized data
+    df = pd.DataFrame(data_for_dataframe)
+
+    # Transpose the DataFrame to have main_keys as columns and small_keys as index
+    df = df.transpose()
+
+    # Fill NaN with an empty string if desired
+    df = df.fillna('')
+
+    df.to_csv('output.csv')
+    print(df)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Data processing script for Energy Forecasting Hackathon')
